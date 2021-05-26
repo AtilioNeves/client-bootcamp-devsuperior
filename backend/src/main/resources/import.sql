e806fa2476b0f7c60271fc0ccd062a4858fd6f7e
AtilioNeves <atilio.neves@accenture.com>
2021-05-26 01:38:32 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tatiane Nair Assis', '95116814866', 5500.0, TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rafaela Gabriela da Luz', '73519594803', 6000.0, TIMESTAMP WITH TIME ZONE '1982-03-21T10:20:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Helena Olivia Eduarda Campos', '18071620874', 8000.0, TIMESTAMP WITH TIME ZONE '1990-02-17T08:10:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joana Fatima Flavia Caldeira', '93784889867', 5000.0, TIMESTAMP WITH TIME ZONE '1981-09-29T11:29:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Melissa Stella Carla Pereira', '71788409868', 7000.0, TIMESTAMP WITH TIME ZONE '1996-04-12T10:01:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Henrique Rodrigo Carvalho', '87329158887', 9700.0, TIMESTAMP WITH TIME ZONE '1988-05-25T08:03:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Anthony Joaquim Drumond', '61939808812', 7300.0, TIMESTAMP WITH TIME ZONE '1982-06-24T09:13:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tomas Matheus Pietro Moraes', '04159460852', 6600.0, TIMESTAMP WITH TIME ZONE '1977-05-19T07:33:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Liz Rosa Ramos', '10658633813', 8200.0, TIMESTAMP WITH TIME ZONE '1984-08-18T11:10:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mariah Benedita Heloise Castro', '69492118807', 5400.0, TIMESTAMP WITH TIME ZONE '1986-07-12T09:53:00Z', 1);
